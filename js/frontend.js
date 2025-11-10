263caa5916d6fb749545e3f8abd73db26ffede0a
sBamboo <85576459+sbamboo@users.noreply.github.com>
2025-11-10 22:26:44 +0100

    Fix mute/unmute

--- js/frontend.js
+++ js/frontend.js
@@ -1,27 +1,29 @@
-// Handle unmute/mute buttons
+// Handle unmute/mute buttons and volume slider
 const playBtn = document.getElementById("audioIcon-paused");
 const pauseBtn = document.getElementById("audioIcon-playing");
+const volumeSlider = document.getElementById("volumeSlider");
 
-if (playBtn && pauseBtn && audio) {
+if (playBtn && pauseBtn && volumeSlider && audio) {
     playBtn.addEventListener("click", () => {
-        audio.allowNewPlays();
-        audio.resumePlaylist("bg.music");
+        // audio.allowNewPlays();
+        // audio.resumePlaylist("bg.music");
+
+        audio.setMasterVolume(parseInt(volumeSlider.value, 10));
+
         playBtn.style.display = "none";
         pauseBtn.style.display = "block";
     });
 
     pauseBtn.addEventListener("click", () => {
-        audio.disAllowNewPlays();
-        audio.pausePlaylist("bg.music");
+        // audio.disAllowNewPlays();
+        // audio.pausePlaylist("bg.music");
+
+        audio.setMasterVolume(0);
+
         pauseBtn.style.display = "none";
         playBtn.style.display = "block";
     });
-}
 
-// Handle volume slider
-const volumeSlider = document.getElementById("volumeSlider");
-
-if (volumeSlider && audio) {
     volumeSlider.addEventListener("input", (event) => {
         const volume = parseInt(event.target.value, 10);
         audio.setMasterVolume(volume);
