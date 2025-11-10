623937b08f4a6e0d464be728a09cc1da50b090c4
sBamboo <85576459+sbamboo@users.noreply.github.com>
2025-11-10 14:33:20 +0100

    Push two tiles

--- js/game.js
+++ js/game.js
@@ -1,3 +1,4 @@
+// Defines
 const TargetFPS = 60;
 let currentFPS = 0;
 let deltaTime = 0;
@@ -5,9 +6,18 @@ let deltaTime = 0;
 const gameCanvas = document.getElementById("canvas");
 const ctx = gameCanvas ? gameCanvas.getContext("2d") : null;
 
-const grid = new Grid(10, 10, 720/16);
 
+// Textures
 const backgroundImg = new Texture("./assets/images/background.png");
+const tileVoid = new Texture("./assets/images/tiles/void.png");
+const tileNonVoidAbove = new Texture("./assets/images/tiles/void-dirt.png");
+
+
+// Instantiate
+const grid = new Grid(10, 10, 720/10);
+grid.setTile(5, 5, new DisabledVoidTile(tileVoid));
+grid.setTile(4, 5, new DisabledVoidTile_NonVoidAbove(tileNonVoidAbove));
+
 
 // Define loops
 function GameLoop(grid) {
